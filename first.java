0b5f46019a5ded794baf1082a28a48c57175e0d9
NygilJohns <146074671+NygilJohns@users.noreply.github.com>
2023-11-08 19:40:00 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,12 @@
+package U_Java;
+import java.util.Scanner;
+public class first {
+	public static void main(String [] args) {
+		String name;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("May i know your name :");
+		name=sc.nextLine();
+		System.out.println("Welcome  "+name);
+	}
+
+}
